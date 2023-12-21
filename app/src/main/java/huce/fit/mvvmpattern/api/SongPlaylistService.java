aab41e166c575339b143e382e9093edb8e3b2ab8
TongDangTu <tongdangtu10102002@gmail.com>
2023-12-21 14:21:36 +0700

    Add, Delete Song in Playlist

---
+++ app/src/main/java/huce/fit/mvvmpattern/api/SongPlaylistService.java
@@ -0,0 +1,28 @@
+package huce.fit.mvvmpattern.api;
+
+import java.util.HashMap;
+
+import huce.fit.mvvmpattern.model.DataJson;
+import huce.fit.mvvmpattern.model.SongPlaylist;
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface SongPlaylistService {
+    SongPlaylistService songPlaylistService = new Retrofit.Builder()
+            .baseUrl("https://nhomhungtu.000webhostapp.com/api/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(SongPlaylistService.class);
+
+    @POST("song_playlist/song_playlist_Add_Favorite.php")
+    Call<DataJson<SongPlaylist>> addToFavorite (@Body HashMap hashMap);
+
+    @POST("song_playlist/song_playlist_Add.php")
+    Call<DataJson<SongPlaylist>> addToPlaylist (@Body HashMap hashMap);
+
+    @POST("song_playlist/song_playlist_Delete.php")
+    Call<DataJson<SongPlaylist>> deleteSongFromPlaylist (@Body HashMap hashMap);
+}
--- app/src/main/java/huce/fit/mvvmpattern/views/PlaylistActivity.java
+++ app/src/main/java/huce/fit/mvvmpattern/views/PlaylistActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -11,7 +12,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
-import android.transition.Transition;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -23,14 +24,19 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 import huce.fit.mvvmpattern.R;
+import huce.fit.mvvmpattern.api.PlaylistService;
 import huce.fit.mvvmpattern.api.SongInfoService;
+import huce.fit.mvvmpattern.api.SongPlaylistService;
 import huce.fit.mvvmpattern.model.DataJson;
 import huce.fit.mvvmpattern.model.Song;
 import huce.fit.mvvmpattern.model.SongInfo;
-import huce.fit.mvvmpattern.services.MediaService;
+import huce.fit.mvvmpattern.model.SongPlaylist;
+import huce.fit.mvvmpattern.utils.Tmp;
+import huce.fit.mvvmpattern.views.adapter.ChoosePlaylistAdapter;
 import huce.fit.mvvmpattern.views.adapter.PlaylistAdapter;
 import huce.fit.mvvmpattern.views.appInterface.IClickSongOption;
 import huce.fit.mvvmpattern.views.fragments.library.itemPlaylist.Playlist;
@@ -76,12 +82,6 @@ public class PlaylistActivity extends AppCompatActivity {
             }
         });
         btnGoBack = findViewById(R.id.btnBack);
-        btnGoBack.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(PlaylistActivity.this, "Back", Toast.LENGTH_SHORT).show();
-            }
-        });
 
         fabAddSongs.setOnClickListener(view -> {
             openMusicPlayer();
@@ -96,7 +96,7 @@ public class PlaylistActivity extends AppCompatActivity {
         adapter.setItems(getPlaylistSong(), new IClickSongOption() {
             @Override
             public void onClickSongOption(Song song) {
-                openSongBottomSheet();
+                openSongBottomSheet(song);
             }
             @Override
             public void onClickSong(Song song) {
@@ -117,12 +117,12 @@ public class PlaylistActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MusicPlayerActivity.class);
         startActivity(intent);
     }
-    public void openSongBottomSheet() {
+    public void openSongBottomSheet(Song song) {
         View viewBottom = getLayoutInflater().inflate(R.layout.layout_bottom_sheet_song_playlist, null);
 
         final BottomSheetDialog dialog = new BottomSheetDialog(this);
         dialog.setContentView(viewBottom);
-        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCanceledOnTouchOutside(true);
 
         ConstraintLayout btnFavorite = dialog.findViewById(R.id.bottom_sheet_options_Favorite);
         ConstraintLayout btnAddToPlaylist = dialog.findViewById(R.id.bottom_sheet_options_Playlist);
@@ -130,23 +130,78 @@ public class PlaylistActivity extends AppCompatActivity {
         btnFavorite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PlaylistActivity.this, "Favorite", Toast.LENGTH_SHORT).show();
+                HashMap<String, String> hashMap = new HashMap<>();
+                hashMap.put("username", Tmp.current_username);
+                hashMap.put("id_song", song.getId());
+                SongPlaylistService.songPlaylistService.addToFavorite(hashMap)
+                        .enqueue(new Callback<DataJson<SongPlaylist>>() {
+                            @Override
+                            public void onResponse(Call<DataJson<SongPlaylist>> call, Response<DataJson<SongPlaylist>> response) {
+                                DataJson<SongPlaylist> dataJson = response.body();
+                                if (dataJson != null) {
+                                    if (dataJson.isStatus() == true) {
+                                        Toast.makeText(PlaylistActivity.this, dataJson.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        Toast.makeText(PlaylistActivity.this, dataJson.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<DataJson<SongPlaylist>> call, Throwable t) {
+                                Log.e("ERROR", this.getClass().getName()+": openSongBottomSheet() -> onFailure(): "+ t.getMessage());
+                            }
+                        });
             }
         });
         btnAddToPlaylist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(PlaylistActivity.this, "Add to playlist", Toast.LENGTH_SHORT).show();
+                dialog.cancel();
+
+                Intent intent = new Intent(PlaylistActivity.this, ChoosePlaylist.class);
+                Bundle bundle = new Bundle();
+                bundle.putSerializable("id_song", song.getId());
+                intent.putExtras(bundle);
+                startActivity(intent);
             }
         });
         btnDelete.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(PlaylistActivity.this, "Delete", Toast.LENGTH_SHORT).show();
+                Bundle bundle = getIntent().getExtras();
+                Playlist playlist = (Playlist) bundle.getSerializable("playlist");
+                HashMap<String, String> hashMap = new HashMap<>();
+                hashMap.put("id_song", song.getId());
+                hashMap.put("id_playlist", playlist.getId());
+                SongPlaylistService.songPlaylistService.deleteSongFromPlaylist(hashMap)
+                        .enqueue(new Callback<DataJson<SongPlaylist>>() {
+                            @Override
+                            public void onResponse(Call<DataJson<SongPlaylist>> call, Response<DataJson<SongPlaylist>> response) {
+                                DataJson<SongPlaylist> dataJson = response.body();
+                                if (dataJson != null) {
+                                    if (dataJson.isStatus() == true) {
+                                        getPlaylistSong();
+                                        Toast.makeText(PlaylistActivity.this, dataJson.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                    else {
+                                        Toast.makeText(PlaylistActivity.this, dataJson.getMessage(), Toast.LENGTH_SHORT).show();
+                                    }
+                                }
+                            }
+
+                            @Override
+                            public void onFailure(Call<DataJson<SongPlaylist>> call, Throwable t) {
+
+                            }
+                        });
             }
         });
         dialog.show();
     }
+
     private List<Song> getPlaylistSong(){
         list = new ArrayList<>();
 //        list.add(new Song("1","https://raw.githubusercontent.com/Tamashi-Dake/Online_Music_Player_Android/main/app/src/main/res/drawable/img_3.jpg","Song 1","Artist 1", "", ""));
@@ -181,7 +236,7 @@ public class PlaylistActivity extends AppCompatActivity {
         adapter.setItems(list, new IClickSongOption() {
             @Override
             public void onClickSongOption(Song song) {
-                openSongBottomSheet();
+                openSongBottomSheet(song);
             }
             @Override
             public void onClickSong(Song song) {
